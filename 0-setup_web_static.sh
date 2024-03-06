9dbdb61c42ccaf5c6b1948402eb1a9088bcd3761
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-03-06 21:46:31 +0200

    Revert "Update 0-setup_web_static.sh"
    This reverts commit 78d96c28c26703be482659eb9a470751ea9ed76c.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,14 @@
 # Prepare your web servers
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt-get updgrade -y
 sudo apt-get install -y nginx
+mkdir -p  /data/web_static/
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
+mkdir /data/web_static/shared/
+sudo echo "Hello AirBnb" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R www-data:www-data /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+chown -R -h ubuntu:ubuntu  /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
+/etc/nginx/sites-available/default
 sudo service nginx restart
