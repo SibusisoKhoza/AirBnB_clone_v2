78d96c28c26703be482659eb9a470751ea9ed76c
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-03-06 21:39:05 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,12 @@
 # Prepare your web servers
 
 sudo apt-get update -y
-sudo apt-get updgrade -y
+sudo apt-get upgrade -y
 sudo apt-get install -y nginx
-mkdir -p  /data/web_static/
 mkdir -p /data/web_static/releases/test/
-mkdir /data/web_static/shared/
-sudo echo "Hello AirBnb" | tee /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R -h ubuntu:ubuntu  /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
-/etc/nginx/sites-available/default
+sudo chown -R www-data:www-data /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
