543dbbaa54aab878a4445a5ca5c70bc119079469
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-03-23 18:20:07 +0200

    Create 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is module 1-hbnb_route.
+It starts a minimal Flask apllication.
+Run it with python3 -m 1-hbnb_route or ./1-hbnb_route
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """flask hello world"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """add a path to the url"""
+    return "HBNB"
